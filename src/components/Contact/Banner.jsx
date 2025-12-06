e27cc3dba0fe71d15760820aaa5c9c3b07afefd3
Jaba-Akter <infojeba218@gmall.com>
2025-12-06 18:55:00 +0600

    question pages add

--- src/components/Contact/Banner.jsx
+++ src/components/Contact/Banner.jsx
@@ -6,10 +6,10 @@ import Icon7 from "../../assets/Contact-assets/Step Icon Container (7).png";
 
 const Banner = () => {
   return (
-    <div>
+    <div className="bg-[#FAFAFA] py-10" >
       <div className=" text-center py-5">
-        <button class=" text-[16px] px-3 py-2 bg-[#F7F7F7] text-gray-500 border border-gray-200 rounded-xl shadow-sm hover:shadow-md transition">
-          About ZedProperty
+        <button class=" text-[16px] px-3 py-2 bg-[#FFFFFF] text-gray-500 border border-gray-200 rounded-xl shadow-sm hover:shadow-md transition">
+          Contact Us
         </button>
         <h1 className=" text-3xl sm:text-[48px] py-5">
           <a
@@ -27,7 +27,7 @@ const Banner = () => {
       </div>
       <div className=" grid sm:grid-cols-3 md:grid-cols-4 gap-5 ">
         {/* 1st card  */}
-        <div className="p-5 border border-gray-100 rounded-xl ">
+        <div className="p-5 border border-gray-100 bg-[#FFFFFF] rounded-xl ">
           <div className=" pb-10">
             <img src={Icon4} alt="" />
           </div>
@@ -40,7 +40,7 @@ const Banner = () => {
           </button>
         </div>
         {/* 2nd card  */}
-        <div className="p-5 border border-gray-100 rounded-xl">
+        <div className="p-5 border border-gray-100 bg-[#FFFFFF] rounded-xl">
           <div className=" pb-10">
             <img src={Icon5} alt="" />
           </div>
@@ -53,7 +53,7 @@ const Banner = () => {
           </button>
         </div>
         {/* 3rd card  */}
-        <div className="p-5 border border-gray-100 rounded-xl">
+        <div className="p-5 border border-gray-100 bg-[#FFFFFF] rounded-xl">
           <div className=" pb-10">
             <img src={Icon6} alt="" />
           </div>
@@ -66,7 +66,7 @@ const Banner = () => {
           </button>
         </div>
         {/* 4th card  */}
-        <div className="p-5 border border-gray-100 rounded-xl">
+        <div className="p-5 border border-gray-100 bg-[#FFFFFF] rounded-xl">
           <div className=" pb-10">
             <img src={Icon7} alt="" />
           </div>
